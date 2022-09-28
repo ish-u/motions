959da738ac83ae30fbc7bd6a12da54e7347fc79a
Anmol Gupta <anmolgupta520@gmail.com>
2022-09-28 20:03:57 +0530

    added redirect to Project Page on click

--- src/components/ProjectTile.tsx
+++ src/components/ProjectTile.tsx
@@ -1,8 +1,15 @@
+import { useLocation } from "wouter";
 import fallback_URL from "../assets/fallback_project.jpg";
 
-const ProjectTile = () => {
+const ProjectTile = ({ name }: { name: string }) => {
+  const [location, setLocation] = useLocation();
   return (
-    <div className="w-1/4 h-64 flex justify-center items-center">
+    <div
+      className="w-1/4 h-64  flex justify-center items-center"
+      onClick={(e) => {
+        setLocation(`/project/${name}`);
+      }}
+    >
       <div className="w-5/6 h-5/6 flex flex-col border-2 border-red-700 rounded-md p-2 bg-red-500/75">
         <div className="h-5/6">
           <img
@@ -12,7 +19,7 @@ const ProjectTile = () => {
           />
         </div>
         <div className="h-1/6 flex items-center px-2 text-lg font-semibold">
-          Project Tile
+          {name}
         </div>
       </div>
     </div>
