f7a42d0c8ef5b25fe47051909f213c035d70624b
Anmol Gupta <anmolgupta520@gmail.com>
2022-09-28 20:02:36 +0530

    added createProject function

--- src/components/CreateProjectTile.tsx
+++ src/components/CreateProjectTile.tsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
+import Loader from "./Loader";
 import Modal from "./Modal";
+import { BaseDirectory } from "@tauri-apps/api/path";
+import { createDir} from "@tauri-apps/api/fs";
 
-const CreateProjectTile = () => {
+const CreateProjectTile = ({ projects }: { projects: string[] }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [projectName, setProjectName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [exists, setExists] = useState(false);
+
+  const createProject = async () => {
+    if (!projects.includes(projectName)) {
+      setLoading(true);
+      await createDir(projectName, { dir: BaseDirectory.App });
+      setIsModalOpen(false);
+      setLoading(false);
+    }
+  };
 
   return (
     <div
@@ -30,35 +44,52 @@ const CreateProjectTile = () => {
       </div>
 
       <Modal isOpen={isModalOpen} setIsOpen={setIsModalOpen}>
-        <div className="px-4">
-          <div className="text-3xl my-2">Create Project</div>
-          <div className="my-6 text-black font-semibold">
-            <input
-              type="text"
-              placeholder="Project Name"
-              className="w-full h-10 p-2 rounded-sm outline-none"
-              onChange={(e) => {
-                setProjectName(e.target.value);
-              }}
-            />
+        <>
+          <div className="px-4">
+            <div className="text-3xl my-2">Create Project</div>
+            <div className="mt-6 text-black font-semibold">
+              <input
+                type="text"
+                placeholder="Project Name"
+                className={`w-full h-10 p-2 rounded-sm outline-none disabled:bg-white/75 ${
+                  exists ? "border-red-500 border-2 " : ""
+                }`}
+                onChange={(e) => {
+                  setProjectName(e.target.value);
+                  setExists(projects.includes(e.target.value));
+                }}
+                disabled={loading}
+              />
+            </div>
+            <div className="text-sm font-semibold my-2">
+              {exists && "name exists"}
+            </div>
+
+            <div>
+              {loading ? (
+                <Loader />
+              ) : (
+                <>
+                  <button
+                    onClick={createProject}
+                    disabled={projectName === "" || exists}
+                    className="disabled:bg-emerald-600 bg-emerald-700/75 hover:bg-emerald-700 p-2 mr-4 my-2 font-semibold rounded-sm text-lg"
+                  >
+                    Create
+                  </button>
+                  <button
+                    onClick={() => {
+                      setIsModalOpen(false);
+                    }}
+                    className="bg-red-500/75 hover:bg-red-500 p-2 mr-4 my-2 font-semibold rounded-sm text-lg"
+                  >
+                    Discard
+                  </button>
+                </>
+              )}
+            </div>
           </div>
-          <div>
-            <button
-              disabled={projectName === ""}
-              className="disabled:bg-emerald-900 bg-emerald-700/75 hover:bg-emerald-700 p-2 mr-4 my-2 font-semibold rounded-sm text-lg"
-            >
-              Create
-            </button>
-            <button
-              onClick={() => {
-                setIsModalOpen(false);
-              }}
-              className="bg-red-500/75 hover:bg-red-500 p-2 mr-4 my-2 font-semibold rounded-sm text-lg"
-            >
-              Discard
-            </button>
-          </div>
-        </div>
+        </>
       </Modal>
     </div>
   );
