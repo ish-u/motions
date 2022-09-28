c30ac92b544940e00b4d8f28eaedae3529063842
Anmol Gupta <anmolgupta520@gmail.com>
2022-09-28 15:48:49 +0530

    added wouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import { useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
-
-import Camera from "./Camera";
+import { Link, Route } from "wouter";
+import Camera from "./pages/Camera";
+import Project from "./pages/Project";
+import Start from "./pages/Start";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
-    <div className="bg-emerald-200 min-h-screen w-full">
-      <Camera />
+    <div className="">
+      <div className="z-50 p-4 w-2/6 flex justify-between">
+        <Link href="/">Start</Link>
+        <Link href="/project">Project</Link>
+
+        <Link href="/camera">Camera</Link>
+      </div>
+
+      <Route path="/" component={Start} />
+      <Route path="/project" component={Project} />
+      <Route path="/camera" component={Camera} />
     </div>
   );
 }
