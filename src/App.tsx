9c74d6969903550d8de546e0ca1cd6a0b4accb21
Anmol Gupta <anmolgupta520@gmail.com>
2022-09-29 19:13:02 +0530

    added back button

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,32 @@ import Start from "./pages/Start";
 
 function App() {
   return (
-    <div className="">
-      <div className="z-50 p-4 w-2/6 flex justify-between">
-        <Link href="/">Start</Link>
-        <Link href="/project">Project</Link>
-
-        <Link href="/camera">Camera</Link>
+    <div className="h-screen w-screen bg-slate-500 px-16 py-8">
+      <div
+        className="fixed top-0 left-0 bg-black/50 p-2 text-white"
+        onClick={() => {
+          console.log("CLICK");
+          window.history.back();
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="h-6 w-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+          />
+        </svg>
       </div>
-
       <Route path="/" component={Start} />
-      <Route path="/project" component={Project} />
-      <Route path="/camera" component={Camera} />
+      <Route path="/project/:name" component={Project} />
+      <Route path="/camera/:name" component={Camera} />
     </div>
   );
 }
