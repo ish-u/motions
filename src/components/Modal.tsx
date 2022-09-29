b0be2dee489dd9efcb43f131f60536676e64a788
Anmol Gupta <anmolgupta520@gmail.com>
2022-09-29 21:18:37 +0530

    changed sizing

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -12,7 +12,7 @@ const Modal = ({
   if (isOpen) {
     return (
       <div
-        className="fixed top-0 left-0 w-screen h-screen bg-black/50 flex justify-center items-center"
+        className="fixed top-0 left-0 flex h-screen w-screen items-center justify-center bg-black/50"
         onClick={(e) => {
           e.stopPropagation();
           setIsOpen(false);
@@ -22,7 +22,7 @@ const Modal = ({
           onClick={(e) => {
             e.stopPropagation();
           }}
-          className="w-2/6 max-h-4/6 border-2 rounded-md border-emerald-800 p-2 bg-emerald-500/90 px-4 py-6"
+          className="max-h-full max-w-full rounded-md border-2 border-emerald-800 bg-emerald-500/90 p-2 px-4 py-6"
         >
           {children}
         </div>
