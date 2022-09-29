9e7be81188b0f63973b3384be7e8bd5b6f6037e0
Anmol Gupta <anmolgupta520@gmail.com>
2022-09-29 19:14:20 +0530

    added pulse animation'

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        pulse: "pulse 2s ease-in-out infinite",
+      },
+      keyframes: {
+        pulse: {
+          "0%": {
+            transform: "scale(0.5)",
+          },
+          "50%": {
+            transform: "scale(1)",
+          },
+          "100%": {
+            transform: "scale(0.5)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
