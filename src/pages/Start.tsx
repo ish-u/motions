3fca8fd3044378c725d353afe9bf0406a1575748
Anmol Gupta <anmolgupta520@gmail.com>
2022-09-30 16:19:41 +0530

    fixed startup

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 import CreateProjectTile from "../components/CreateProjectTile";
 import ProjectTile from "../components/ProjectTile";
-import { readDir, BaseDirectory } from "@tauri-apps/api/fs";
+import { readDir, BaseDirectory, exists, createDir } from "@tauri-apps/api/fs";
 
 const Start = () => {
   const [projects, setProjects] = useState<string[]>([]);
 
   useEffect(() => {
     const getDirectories = async () => {
+      const check = (await exists("", { dir: BaseDirectory.App })) as unknown;
+      if (check === false) {
+        await createDir("", { dir: BaseDirectory.App });
+      }
       const directories = (await readDir("", { dir: BaseDirectory.App }))
         .map((dir) => (dir.name && dir.children ? dir.name : ""))
         .filter((dir) => dir !== "");
@@ -18,8 +22,8 @@ const Start = () => {
   }, []);
 
   return (
-    <div className="w-full h-full overflow-hidden bg-red-500/50 text-white">
-      <div className="flex flex-wrap overflow-y-scroll max-h-full p-8">
+    <div className="h-full w-full overflow-hidden bg-red-500/50 text-white">
+      <div className="flex max-h-full flex-wrap overflow-y-scroll p-8">
         <CreateProjectTile projects={projects} />
         {projects.map((project, idx) => (
           <ProjectTile name={project} key={idx} />
